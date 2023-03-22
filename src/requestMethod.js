a8d79ba90f3c24cf07cc6779f422408fb0af9153
Jamal Ismail <77586693+jamalgabir@users.noreply.github.com>
2023-03-22 16:11:20 +0000

    Update requestMethod.js
    Added New Fetch Url

--- src/requestMethod.js
+++ src/requestMethod.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "https://ecommirce-app.herokuapp.com/";
+const BASE_URL = "https://ecommerce--app.herokuapp.com/";
 const token = "hello"??JSON.parse(JSON.parse(localStorage.getItem("persist:root"))?.user)?.currentUser?.accessToken;
 export const puplicReuest = axios.create({
     baseURL:BASE_URL,

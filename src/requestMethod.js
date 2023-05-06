2d706cda0a43cd74e6ecd14878e8c6be137ea32a
jamalgabir <jamalgamy@gmail.com>
2023-05-06 21:37:21 +0100

    update

--- src/requestMethod.js
+++ src/requestMethod.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const BASE_URL = "http://localhost:5000/api";
-const token =JSON.parse(JSON.parse(localStorage.getItem("persist:root"))?.user).currentUser?.accessToken;
-console.log(token)
+const token =JSON.parse(JSON.parse(localStorage.getItem("persist:root"))?.user).currentUser?.accessToken||null;
+
 export const puplicReuest = axios.create({
     baseURL:BASE_URL,
 

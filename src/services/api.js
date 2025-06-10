7ed316ec284330efbfac7f3371dfc466c9a536ca
Jamal Ismail <77586693+jamalgabir@users.noreply.github.com>
2025-06-10 08:22:35 +0100

    Updated api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-const BASE_URL = process.env.REACT_APP_API_URL || 'https://ecommirce-api.onrender.com';
+const BASE_URL = process.env.REACT_APP_API_URL || 'localhost:5000';
+const BASE_URL = process.env.REACT_APP_API_URL || 'localhost:5000';
 
 // Public API instance
 export const publicRequest = axios.create({

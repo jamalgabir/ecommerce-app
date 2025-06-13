db838ee2024726913d1aa2f25b3ffa591b0b5bac
Jamal Ismail <77586693+jamalgabir@users.noreply.github.com>
2025-06-13 18:19:25 +0100

    Updated api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = "http://localhost:5000";
+const BASE_URL = "http://localhost:5000/api";
 
 // Public API instance
 export const publicRequest = axios.create({
